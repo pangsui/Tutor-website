f4b31005837a4efdb3a20ab2e9578f05db797429
pangsui <upangsuilingeh@gmail.com>
2024-08-20 10:02:50 +0200

    revealing sections

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
+'use strict';
 // toggling nav bar on mobile devices
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
-    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
-    
-    toggleButton.addEventListener('click', function () {
-        mobileMenu.classList.toggle('active');
-    })
-})
+const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
+const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+toggleButton.addEventListener('click', function () {
+  mobileMenu.classList.toggle('active');
+});
 
 // changing navbar upon scrolling
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', function () {
-    console.log(window.scrollY)
-    if (window.scrollY > 0) {
-        navbar.classList.add('navbar-scroll');  
-    } else {
-        navbar.classList.remove('navbar-scroll');
-    }
-})
+  // console.log(window.scrollY);
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+});
+
+// animation by revealing sections
+const allsections = document.querySelectorAll('.sec');
+
+const obsCallback = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  // observer.unobserve(entry.target);
+};
+const observer = new IntersectionObserver(obsCallback, {
+  root: null,
+  threshold: 0.15, //section reveal when it is 15% visible
+});
+allsections.forEach(function (sect) {
+  observer.observe(sect);
+  sect.classList.add('section--hidden');
+});
